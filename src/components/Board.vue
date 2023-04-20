d158ecd90ac8fb18378c1a6df360a2d4c2c51b71
Alapan Chatterjee <chatterjee.alapan07@gmail.com>
2023-04-21 00:32:30 +0530

    chore: implement basic state management in Board component.

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,23 +1,32 @@
 <script setup>
+import { ref } from 'vue'
 import Square from './Square.vue'
+
+const squares = ref(Array(9).fill(null))
+
+function setSquares(index) {
+  let clonedSquares = squares.value.slice()
+  clonedSquares[index] = 'X'
+  squares.value = clonedSquares
+}
 </script>
 
 <template>
   <div class="board">
     <div class="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square :value="squares[0]" @on-click-square="setSquares(0)" />
+      <Square :value="squares[1]" @on-click-square="setSquares(1)" />
+      <Square :value="squares[2]" @on-click-square="setSquares(2)" />
     </div>
     <div class="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square :value="squares[3]" @on-click-square="setSquares(3)" />
+      <Square :value="squares[4]" @on-click-square="setSquares(4)" />
+      <Square :value="squares[5]" @on-click-square="setSquares(5)" />
     </div>
     <div class="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square :value="squares[6]" @on-click-square="setSquares(6)" />
+      <Square :value="squares[7]" @on-click-square="setSquares(7)" />
+      <Square :value="squares[8]" @on-click-square="setSquares(8)" />
     </div>
   </div>
 </template>
--- src/components/Square.vue
+++ src/components/Square.vue
@@ -1,11 +1,22 @@
+<script setup>
+defineProps({
+  value: {
+    type: String,
+    default: ''
+  }
+})
+const emit = defineEmits(['on-click-square'])
+</script>
+
 <template>
-  <button class="square">X</button>
+  <button class="square" @click="emit('on-click-square')">{{ value }}</button>
 </template>
 
 <style scoped>
 .square {
   height: 50px;
   width: 50px;
+  float: left;
   border: solid 1px #999;
   background-color: #fff;
   font-size: 24px;
